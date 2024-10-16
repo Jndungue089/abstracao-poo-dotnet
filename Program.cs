45181a28bf506ebe8c1741fc9f551841a28556f1
Jndungue089 <68353090+Jndungue089@users.noreply.github.com>
2024-10-16 12:26:55 +0100

    Challenge Done!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia= new Nokia("9000000");
+Iphone iphone= new Iphone("9000000");
+nokia.InstalarAplicativo("WhatsApp");
+iphone.InstalarAplicativo("WhatsApp");
